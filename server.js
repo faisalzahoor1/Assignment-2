16e1e50c6dd465b60fc3a85ffcc074966387e835
Muhammad Faisal Zahoor <152835077+faisalzahoor1@users.noreply.github.com>
2026-04-13 01:23:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express()
-const port = 8080
+const port = 6000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
